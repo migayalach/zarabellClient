1e2287fa964f5b6c4ea38b9dfddf4d03e1521087
migayalach <ayalachavezmiguel@gmail.com>
2026-04-15 23:50:27 -0400

    creted stoker to roles

---
+++ features/roles/hooks/userRequireRole.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { getMonthPurchaseBuys } from "../store/role.slice";
+
+import { MonthBuyData } from "../types";
+
+export const useMonthPBuys = () => {
+  const dispatch = useAppDispatch();
+  const purchaseBuy = useAppSelector((state) => state.monthPurchaseBuy);
+
+  const getMonthPB = (data: MonthBuyData) => {
+    dispatch(getMonthPurchaseBuys(data));
+  };
+
+  return {
+    getMonthPB,
+    loading: purchaseBuy.loading,
+    error: purchaseBuy.error,
+  };
+};
---
+++ features/roles/store/index.ts
@@ -0,0 +1,9 @@
+export { default as monthPurchaseBuyReducer } from "./role.slice";
+
+export { getMonthPurchaseBuys, clearInfoMonthPurchaseBuy } from "./role.slice";
+
+export {
+  selectMonthPurchaseBuysResults,
+  selectMonthPurchaseBuysLoading,
+  selectMonthPurchaseBuysError,
+} from "./role.selectors";
