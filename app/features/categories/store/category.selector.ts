97c9fa2144d0b7357d8eb417016bde443b7d80a0
migayalach <ayalachavezmiguel@gmail.com>
2026-06-11 21:27:52 -0400

    create new selector to category

---
+++ app/features/categories/store/category.selector.ts
@@ -0,0 +1,8 @@
+import { RootState } from "@/app/store/store";
+
+export const selectCategoryInfo = (state: RootState) => state.categories.info;
+export const selectCategoriesResults = (state: RootState) => state.categories.results;
+export const selectCategoryLoading = (state: RootState) => state.categories.loading;
+export const selectCategoryError = (state: RootState) => state.categories.error;
+export const selectCurrentCategory = (state: RootState) => state.categories.currentCategory;
+export const selectSuccesCategoryFlag = (state: RootState) => state.categories.success;
