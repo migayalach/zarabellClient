5d6e8cf3186d2e4ae092bf1ef29126e3ded342f1
migayalach <ayalachavezmiguel@gmail.com>
2026-06-11 21:24:09 -0400

    create interfaces to roles

--- app/features/roles/types.ts
+++ app/features/roles/types.ts
@@ -1,16 +1,36 @@
-export interface RoleInfo {
+export interface IRole {
   idRole: number;
   nameRole: string;
 }
 
-export interface InfoData {
+export interface IPaginationRole {
   count: number;
   pages: number;
   next: number | null;
   prev: number | null;
 }
 
-export interface PaginateResponse {
-  info: InfoData;
-  results: RoleInfo[];
+export interface IErrorRole {
+  success: false;
+  message: string;
 }
+
+export interface IApiResponse<T> {
+  success: boolean;
+  message: string;
+  results: T;
+}
+export interface IResponseRole {
+  success: boolean;
+  message: string;
+  info: IPaginationRole;
+  results: IRole[];
+}
+
+export interface IResponseRole {
+  success: boolean;
+  message: string;
+  value: IRole;
+}
+
+export type IClientSingleResponse = IApiResponse<IRole>;
