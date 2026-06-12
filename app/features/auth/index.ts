4eab5b35db31290500ddbb87bdc6bfbeefad8da8
migayalach <ayalachavezmiguel@gmail.com>
2026-06-11 21:30:25 -0400

    delete files

--- app/features/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./store/auth.slice";
-export * from "./store/auth.selector";
-export * from "./types";
--- app/features/roles/store/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as monthPurchaseBuyReducer } from "./role.slice";
-
-export { getRole, clearInfoRol } from "./role.slice";
-
-export {
-  selectMonthPurchaseBuysResults,
-  selectMonthPurchaseBuysLoading,
-  selectMonthPurchaseBuysError,
-} from "./role.selectors";
