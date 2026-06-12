7e598f1bfb2440d0b3fb94334be182ae5887440a
migayalach <ayalachavezmiguel@gmail.com>
2026-06-11 21:19:55 -0400

    added new reducers roles, users, categories, products

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/app/features/auth/store/auth.slice";
+import rolesReducer from "@/app/features/roles/store/role.slice";
+import usersReducer from "@/app/features/users/store/user.slice";
+import categories from "@/app/features/categories/store/category.slice";
+import products from "@/app/features/products/store/product.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    roles: rolesReducer,
+    users: usersReducer,
+    categories: categories,
+    products: products,
   },
 });
 
