0461b3f6849c0b0c30c794c370adeab5fb6feabc
migayalach <ayalachavezmiguel@gmail.com>
2026-06-13 17:17:47 -0400

    delete success state

--- app/features/roles/types.ts
+++ app/features/roles/types.ts
@@ -11,24 +11,20 @@ export interface IPaginationRole {
 }
 
 export interface IErrorRole {
-  // success: false;
   message: string;
 }
 
 export interface IApiResponse<T> {
-  // success: boolean;
   message: string;
   results: T;
 }
 export interface IResponseRoles {
-  // success: boolean;
   message: string;
   info: IPaginationRole;
   results: IRole[];
 }
 
 export interface IResponseRole {
-  // success: boolean;
   message: string;
   value: IRole;
 }
