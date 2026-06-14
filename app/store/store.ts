004eb1b84b636f4d649b2419cc2aaac09062dabc
migayalach <ayalachavezmiguel@gmail.com>
2026-06-14 16:14:56 -0400

    import new slices to use

--- app/store/store.ts
+++ app/store/store.ts
@@ -4,6 +4,9 @@ import rolesReducer from "@/app/features/roles/store/role.slice";
 import usersReducer from "@/app/features/users/store/user.slice";
 import categories from "@/app/features/categories/store/category.slice";
 import products from "@/app/features/products/store/product.slice";
+import providers from "@/app/features/providers/store/provider.slice";
+import reasons from "@/app/features/reasons/store/reason.slice";
+import typeOutputs from "@/app/features/typeOutputs/store/typeOutput.slice";
 
 export const store = configureStore({
   reducer: {
@@ -12,6 +15,9 @@ export const store = configureStore({
     users: usersReducer,
     categories: categories,
     products: products,
+    providers: providers,
+    reasons: reasons,
+    typeOutputs: typeOutputs,
   },
 });
 
