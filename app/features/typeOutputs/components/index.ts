5d0182ad0d93d09d3e5a6b3cf1861b5a1bf426e2
migayalach <ayalachavezmiguel@gmail.com>
2026-06-14 19:01:25 -0400

    export components from type outputs

---
+++ app/features/typeOutputs/components/index.ts
@@ -0,0 +1,3 @@
+export { default as OutputTypeButtonModal } from "./OutputTypeButtonModal";
+export { default as OutputTypePagination } from "./OutputTypePagination";
+export { default as OutputTypeTable } from "./OutputTypeTable";
