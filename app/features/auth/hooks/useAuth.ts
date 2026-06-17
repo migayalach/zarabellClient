4631f26a178ab523d1d386b18f73f38f32f6826b
migayalach <ayalachavezmiguel@gmail.com>
2026-06-17 19:53:11 -0400

    return initialized

--- app/features/auth/hooks/useAuth.ts
+++ app/features/auth/hooks/useAuth.ts
@@ -3,14 +3,14 @@ import {
   selectAuthUser,
   selectAuthLoading,
   selectAuthError,
-  selectIsAuthenticated,
+  selectInitialized,
 } from "../store/auth.selector";
 
 export const useAuth = () => {
   const user = useAppSelector(selectAuthUser);
   const loading = useAppSelector(selectAuthLoading);
   const error = useAppSelector(selectAuthError);
-  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const initialized = useAppSelector(selectInitialized);
 
-  return { user, loading, isAuthenticated, error };
+  return { user, loading, error, initialized };
 };
