e3ac088e09cb0efa71f5a251910aa8ad929a349b
migayalach <ayalachavezmiguel@gmail.com>
2026-06-17 19:16:34 -0400

    add new function to get information about user

--- app/features/auth/services/auth.services.ts
+++ app/features/auth/services/auth.services.ts
@@ -1,11 +1,59 @@
 import { api } from "@/app/shared/api/axios";
-import { UserStore } from "../types";
+import { IResponseCurrentUser, SignInResponse, UserStore } from "../types";
 
-export const signInRequest = async (data: UserStore) => {
-  const response = await api.post("api/auth/signIn", {
-    emailUser: data.email,
-    passwordUser: data.password,
+export const signInRequest = async (
+  data: UserStore,
+): Promise<SignInResponse> => {
+  const response = await api.post("", {
+    query: `
+      mutation($emailUser: String!, $passwordUser: String!){
+      singIn(userInfo:{
+        emailUser: $emailUser
+        passwordUser: $passwordUser
+      }){
+        success
+        message
+        access_token
+        value {
+          idUser
+          idRole
+          nameRole
+          nameUser
+          lastNameUser
+          emailUser
+          phoneUser
+        }
+      }
+    }
+    `,
+    variables: {
+      emailUser: data.email,
+      passwordUser: data.password,
+    },
   });
 
-  return response.data;
+  return response.data.data.singIn;
+};
+
+export const getCurrentInfoUser = async (): Promise<IResponseCurrentUser> => {
+  const response = await api.post("", {
+    query: `
+     query {
+      currentUser {
+        success
+        value {
+          idUser
+          idRole
+          nameRole
+          nameUser
+          lastNameUser
+          emailUser
+          phoneUser
+        }
+      }
+    }
+    `,
+  });
+
+  return response.data.data.currentUser;
 };
