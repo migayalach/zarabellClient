3a98cd4a5dd2d51e4526da0fb32c88ba6beec099
migayalach <ayalachavezmiguel@gmail.com>
2026-06-18 19:47:16 -0400

    create new functions to call update and change password

--- app/features/auth/services/auth.services.ts
+++ app/features/auth/services/auth.services.ts
@@ -1,5 +1,11 @@
 import { api } from "@/app/shared/api/axios";
-import { IResponseCurrentUser, SignInResponse, UserStore } from "../types";
+import {
+  IPassword,
+  IResponseCurrentUser,
+  IUpdateProfile,
+  SignInResponse,
+  UserStore,
+} from "../types";
 
 export const signInRequest = async (
   data: UserStore,
@@ -57,3 +63,61 @@ export const getCurrentInfoUser = async (): Promise<IResponseCurrentUser> => {
 
   return response.data.data.currentUser;
 };
+
+export const changePassword = async (data: IPassword) => {
+  const response = await api.post("", {
+    query: `
+    mutation($currentPassword: String!, $newPassword: String!){
+      changePassword(infoPassword:{
+        currentPassword: $currentPassword
+        newPassword: $newPassword
+      }){
+        success
+        message
+      }
+    }
+    `,
+    variables: {
+      currentPassword: data.currentPassword,
+      newPassword: data.newPassword,
+    },
+  });
+
+  return response.data.data.changePassword;
+};
+
+export const updateInfoUser = async (data: IUpdateProfile) => {
+  const response = await api.post("", {
+    query: `
+      mutation($nameUser: String!, $lastNameUser: String!, $emailUser: String!,  $phoneUser: String!){
+        updateCurrentProfile(infoProfile:{
+          nameUser: $nameUser
+          lastNameUser: $lastNameUser
+          emailUser: $emailUser
+          phoneUser: $phoneUser   
+        }){
+          success
+          value {
+              idUser
+              idRole
+              nameRole
+              nameUser
+              lastNameUser
+              emailUser
+              phoneUser
+          }
+        }
+      }
+    `,
+    variables: {
+      nameUser: data.nameUser,
+      lastNameUser: data.lastNameUser,
+      emailUser: data.emailUser,
+      phoneUser: data.phoneUser,
+    },
+  });
+
+  return response.data.data.updateCurrentProfile;
+};
+
+export const resetPassword = () => {};
