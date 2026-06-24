79d2fdc4f25e81f3253232a97859c4654934da26
migayalach <ayalachavezmiguel@gmail.com>
2026-06-24 18:54:42 -0400

    implement components

--- app/(client)/inventory-entries/[idInventoryEntry]/page.tsx
+++ app/(client)/inventory-entries/[idInventoryEntry]/page.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import {
+  usePriceHistory,
+  usePriceHistoryActions,
+} from "@/app/features/priceHistory/hooks";
+import {
+  PriceHistoryModalAction,
+  PriceHistoryPagination,
+  PriceHistoryTable,
+} from "@/app/features/priceHistory/components";
 
-function Page() {
+interface IParams {
+  params: Promise<{
+    idInventoryEntry: string;
+  }>;
+}
+
+function Page({ params }: IParams) {
+  const { idInventoryEntry } = React.use(params);
+  const { info, results } = usePriceHistory();
+  const { getAllPriceHistory } = usePriceHistoryActions();
+
+  useEffect(() => {
+    getAllPriceHistory(+idInventoryEntry);
+  }, []);
+
   return (
-    <div>
-      <h1>Actualizacion de precios</h1>
+    <div className="flex flex-col flex-1">
+      <div className="flex items-center justify-between">
+        <h1 className="text-[30px] font-bold">Lista de entradas</h1>
+        {/* <InputRecordButtonModal text="Crear" action="create" /> */}
+        {/* <PriceHistoryModalAction /> */}
+      </div>
+
+      <div className="flex-1 mt-2">
+        <PriceHistoryTable idInputRecord={+idInventoryEntry} infoPH={results} />
+      </div>
+
+      <div className="flex justify-center mt-6">
+        {/* <InputRecordPagination pages={info?.pages ?? 1} /> */}
+        <PriceHistoryPagination />
+      </div>
     </div>
   );
 }
