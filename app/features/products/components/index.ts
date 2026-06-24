4e666655ac6cc316cb9e2f8c21c58d9d1d5a7f30
migayalach <ayalachavezmiguel@gmail.com>
2026-06-24 19:29:28 -0400

    export components

--- app/features/products/components/index.ts
+++ app/features/products/components/index.ts
@@ -1,4 +1,4 @@
 export { default as ProductButtonModal } from "./ProductButtonModal";
 export { default as ProductPagination } from "./ProductPagination";
 export { default as ProductTable } from "./ProductTable";
-export { default as ProductSelect } from "./ProductSelect";
+export { default as ProductList } from "./ProductList";
