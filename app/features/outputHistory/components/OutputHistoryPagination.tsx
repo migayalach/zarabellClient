a5a5b75035e8bf6dd8726822f5211398c898738c
migayalach <ayalachavezmiguel@gmail.com>
2026-07-07 21:17:15 -0400

    add logic

--- app/features/outputHistory/components/OutputHistoryPagination.tsx
+++ app/features/outputHistory/components/OutputHistoryPagination.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import { Pagination } from "antd";
+import { useOutputHistory, useOutputHistoryActions } from "../hooks";
 
-function OutputHistoryPagination() {
+function OutputHistoryPagination({
+  idOutput,
+  pages,
+}: {
+  idOutput: number;
+  pages: number;
+}) {
+  const [current, setCurrent] = useState(1);
+  const [index, setIndex] = useState(0);
+  const { getAllOutputHistory, resetActionCreateUpdate, clearErrorInfo } =
+    useOutputHistoryActions();
+  const { info, results, currentOutputHistory, success, error } =
+    useOutputHistory();
+
+  const onChange = (page: number) => {
+    setCurrent(page);
+    getAllOutputHistory(idOutput, page);
+  };
+
+  useEffect(() => {
+    setIndex(results.length + 1);
+  }, [results]);
+
+  useEffect(() => {
+    if (currentOutputHistory && success) {
+      resetActionCreateUpdate();
+      if (index < 21) {
+        setCurrent(info!.pages);
+        getAllOutputHistory(idOutput, info?.pages);
+      } else {
+        const tempCount = current + 1;
+        setCurrent(tempCount);
+        getAllOutputHistory(idOutput, tempCount);
+      }
+    } else if (success) {
+      setTimeout(() => {
+        resetActionCreateUpdate();
+      }, 1000);
+      getAllOutputHistory(idOutput, current);
+    }
+  }, [currentOutputHistory, success, index]);
+
+  useEffect(() => {
+    if (error) {
+      clearErrorInfo();
+    }
+  }, [error]);
+
   return (
-    <div>OutputHistoryPagination</div>
-  )
+    <Pagination
+      onChange={onChange}
+      total={pages * 10}
+      current={current}
+      showSizeChanger={false}
+    />
+  );
 }
 
-export default OutputHistoryPagination
+export default OutputHistoryPagination;
