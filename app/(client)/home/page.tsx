e85a35433525cf1df33be3a185e749f716ffa362
migayalach <ayalachavezmiguel@gmail.com>
2026-07-27 20:19:00 -0400

    import components to dashboard

--- app/(client)/home/page.tsx
+++ app/(client)/home/page.tsx
@@ -1,11 +1,65 @@
-import React from "react";
+"use client";
 
-function Page() {
+import "./dashboard.css";
+
+import {
+  BestSellers,
+  LessSellers,
+  NearExpiration,
+  SalesReportToday,
+  SalesReportYesterday,
+  TotalReport,
+} from "@/app/features/dashboard/components";
+import { useDashboard } from "@/app/features/dashboard/hooks/useDashoard";
+import { useEffect } from "react";
+import { FormOutlined } from "@ant-design/icons";
+
+export default function Page() {
+  const {
+    bestSellers,
+    lessSellers,
+    nearExpiration,
+    todaySales,
+    yesterdaySales,
+    totalProducts,
+    totalOutputsCurrentMonth,
+    totalLotsToExpiration,
+    fetchDashboardInfo,
+  } = useDashboard();
+
+  useEffect(() => {
+    fetchDashboardInfo();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="dashboard">
+      <header className="mb-6 text-left max-sm:text-center">
+        <h1 className="text-4xl font-bold text-gray-800">
+          Dashboard <FormOutlined />
+        </h1>
+      </header>
+
+      <section className="flex flex-wrap gap-6 justify-center">
+        {todaySales && <SalesReportToday info={todaySales} />}
+        {yesterdaySales && <SalesReportYesterday info={yesterdaySales} />}
+        <TotalReport text="productos" info={totalProducts} />
+        <TotalReport text="ventas" info={totalOutputsCurrentMonth} />
+        <TotalReport text="lotes" info={totalLotsToExpiration} />
+      </section>
+
+      <section className="dashboard-charts">
+        <div className="dashboard-card">
+          <BestSellers info={bestSellers} />
+        </div>
+
+        <div className="dashboard-card">
+          <LessSellers info={lessSellers} />
+        </div>
+
+        <div className="dashboard-card">
+          <NearExpiration info={nearExpiration} />
+        </div>
+      </section>
+    </main>
   );
 }
-
-export default Page;
