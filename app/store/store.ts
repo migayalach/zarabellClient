542f0c44098af0324d163d64a57fb4f7caa58b9e
migayalach <ayalachavezmiguel@gmail.com>
2026-07-27 20:18:25 -0400

    import new element to store and rename reducers

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "@/app/features/auth/store/auth.slice";
-import rolesReducer from "@/app/features/roles/store/role.slice";
-import usersReducer from "@/app/features/users/store/user.slice";
+import auth from "@/app/features/auth/store/auth.slice";
+import roles from "@/app/features/roles/store/role.slice";
+import users from "@/app/features/users/store/user.slice";
 import categories from "@/app/features/categories/store/category.slice";
 import products from "@/app/features/products/store/product.slice";
 import providers from "@/app/features/providers/store/provider.slice";
@@ -11,21 +11,23 @@ import inputRecords from "@/app/features/inputRecord/store/entriesInventory.slic
 import priceHistory from "@/app/features/priceHistory/store/priceHistory.slice";
 import outputs from "@/app/features/outputsInventory/store/outputsInventory.slice";
 import outputHistory from "@/app/features/outputHistory/store/outputHistory.slice";
+import dashboard from "@/app/features/dashboard/store/dashboard.slice";
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    roles: rolesReducer,
-    users: usersReducer,
-    categories: categories,
-    products: products,
-    providers: providers,
-    reasons: reasons,
-    typeOutputs: typeOutputs,
-    inputRecords: inputRecords,
-    priceHistory: priceHistory,
-    outputs: outputs,
-    outputHistory: outputHistory,
+    auth,
+    roles,
+    users,
+    categories,
+    products,
+    providers,
+    reasons,
+    typeOutputs,
+    inputRecords,
+    priceHistory,
+    outputs,
+    outputHistory,
+    dashboard,
   },
   // devTools: process.env.AROUND_DEVELOP === "develop",
 });
