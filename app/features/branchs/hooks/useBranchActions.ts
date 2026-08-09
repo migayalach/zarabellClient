d4176ebf36091c73196b3b9d85312532a58bffbb
migayalach <ayalachavezmiguel@gmail.com>
2026-08-08 22:12:36 -0400

    create hook to actions

---
+++ app/features/branchs/hooks/useBranchActions.ts
@@ -0,0 +1,78 @@
+"use client";
+import { useAppDispatch } from "@/app/store/hooks";
+import {
+  clearCurrentBranchData,
+  clearInfoBranchError,
+  createBranch,
+  deleteOneBranchByID,
+  getAllListBranchs,
+  getBranchByID,
+  resetAllDataBranch,
+  resetBranchCreateUpdateData,
+  resetStateActionBranch,
+  updateOneBranchByID,
+} from "../store/branch.slice";
+import { IBranchCreate, IBranchUpdate } from "../types";
+import { unwrapResult } from "@reduxjs/toolkit";
+
+export const useBranchsActions = () => {
+  const dispatch = useAppDispatch();
+
+  const getAllBranchs = async (page?: number) => {
+    const result = await dispatch(getAllListBranchs(page));
+    return unwrapResult(result);
+  };
+
+  const getOneBranchByID = async (idBranch: number) => {
+    const result = await dispatch(getBranchByID(idBranch));
+    return unwrapResult(result);
+  };
+
+  const createNewBranch = async (data: IBranchCreate) => {
+    const result = await dispatch(createBranch(data));
+    return unwrapResult(result);
+  };
+
+  const updateBranch = async (data: IBranchUpdate) => {
+    const result = await dispatch(updateOneBranchByID(data));
+    return unwrapResult(result);
+  };
+
+  const deleteBranch = async (idBranch: number) => {
+    const result = await dispatch(deleteOneBranchByID(idBranch));
+    return unwrapResult(result);
+  };
+
+  const clearCurrentBranch = () => {
+    dispatch(clearCurrentBranchData());
+  };
+
+  const resetBranch = () => {
+    dispatch(resetAllDataBranch());
+  };
+
+  const resetBranchActCreateUpdate = () => {
+    dispatch(resetBranchCreateUpdateData());
+  };
+
+  const clearErrorBranch = () => {
+    dispatch(clearInfoBranchError());
+  };
+
+  const resetBranchActionState = () => {
+    dispatch(resetStateActionBranch());
+  };
+
+  return {
+    getAllBranchs,
+    getOneBranchByID,
+    createNewBranch,
+    updateBranch,
+    deleteBranch,
+    clearCurrentBranch,
+    resetBranch,
+    resetBranchActCreateUpdate,
+    clearErrorBranch,
+    resetBranchActionState,
+  };
+};
