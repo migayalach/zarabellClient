2badd6e59ac1a36e516fc799735b93e956b2a392
migayalach <ayalachavezmiguel@gmail.com>
2026-08-27 12:43:51 -0400

    feat: update user store

--- app/features/users/store/user.slice.ts
+++ app/features/users/store/user.slice.ts
@@ -5,6 +5,7 @@ import {
   deleteOneUser,
   getAllUsers,
   getOneUserByID,
+  filterUsers,
 } from "../services/users.services";
 import {
   IErrorUser,
@@ -14,6 +15,7 @@ import {
   IUserInfo,
   IUserUpdate,
   TUserActionWatch,
+  IFilterUser,
 } from "../types";
 
 interface IUserState {
@@ -22,6 +24,7 @@ interface IUserState {
   currentUser: IUserInfo | null;
   loading: boolean;
   error: string | null;
+  success: boolean;
   actionWatch: TUserActionWatch | null;
 }
 
@@ -31,6 +34,7 @@ const initialState: IUserState = {
   currentUser: null,
   loading: false,
   error: null,
+  success: false,
   actionWatch: null,
 };
 
@@ -89,6 +93,21 @@ export const deleteOneUserByID = createAsyncThunk<
 >("users/deleteUser", async (idUser, { rejectWithValue }) => {
   try {
     return await deleteOneUser(idUser);
+  } catch (error) {
+    return rejectWithValue(error as IErrorUser);
+  }
+});
+
+export const userFilters = createAsyncThunk<
+  IResponseUsers,
+  {
+    filters?: IFilterUser;
+    page?: number | undefined;
+  },
+  { rejectValue: IErrorUser }
+>("users/filter", async ({ filters, page }, { rejectWithValue }) => {
+  try {
+    return await filterUsers(filters, page);
   } catch (error) {
     return rejectWithValue(error as IErrorUser);
   }
@@ -192,6 +211,22 @@ const userSlice = createSlice({
         // state.results = state.results.filter((item) => item.idUser !== idUser);
       })
       .addCase(deleteOneUserByID.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message ?? "Error";
+      })
+
+      // TODO FILTERS
+      .addCase(userFilters.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(userFilters.fulfilled, (state, action) => {
+        state.loading = false;
+        state.info = action.payload.info;
+        state.results = action.payload.results;
+        state.actionWatch = "filters";
+      })
+      .addCase(userFilters.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload?.message ?? "Error";
       });
