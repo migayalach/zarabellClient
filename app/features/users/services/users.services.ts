a5bd84d81a2c9b9c3d1884334e700d1d048e9033
migayalach <ayalachavezmiguel@gmail.com>
2026-08-27 12:43:49 -0400

    feat: update users service

--- app/features/users/services/users.services.ts
+++ app/features/users/services/users.services.ts
@@ -1,5 +1,5 @@
 import { api } from "@/app/shared/api/axios";
-import { IUserCreate, IUserUpdate } from "../types";
+import { IFilterUser, IUserCreate, IUserUpdate } from "../types";
 
 export const getAllUsers = async (page?: number) => {
   const response = await api.post("", {
@@ -181,4 +181,53 @@ export const deleteOneUser = async (idUser: number) => {
   });
 
   return response.data.data.deleteUser;
+};
+
+export const filterUsers = async (
+  filterUsers?: IFilterUser,
+  page?: number,
+) => {
+  const response = await api.post("", {
+    query: `
+      query filterUsers($page: Int, $idRol: Int, $nameUser: String, $emailUser: String, $stateUser: Boolean, $order: Order) {
+        filterUsers(
+          page: $page
+          filters: {
+            idRol: $idRol
+            nameUser: $nameUser
+            emailUser: $emailUser
+            stateUser: $stateUser
+            order: $order
+          }
+        ) {
+          info {
+            pages
+            count
+            next
+            prev
+          }
+          results {
+            idUser
+            idRole
+            nameRole
+            nameUser
+            lastNameUser
+            emailUser
+            phoneUser
+            stateUser
+          }
+        }
+      }
+    `,
+    variables: {
+      page,
+      idRol: filterUsers?.idRol || undefined,
+      nameUser: filterUsers?.nameUser || undefined,
+      emailUser: filterUsers?.emailUser || undefined,
+      stateUser: filterUsers?.stateUser ?? undefined,
+      order: filterUsers?.order ?? "ASC",
+    },
+  });
+
+  return response.data.data.filterUsers;
 };
